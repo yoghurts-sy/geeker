6f718f9a11f2855f93c4100e84d8353e36e282da
JustFine <2215986391@qq.com>
2021-11-10 22:10:16 +0800

    列表组件构建 by yyq

--- geeker-uniapp/main.js
+++ geeker-uniapp/main.js
@@ -3,6 +3,9 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+import divider from '@/components/common/divider.vue';
+Vue.component('divider', divider)
+
 App.mpType = 'app'
 
 const app = new Vue({
